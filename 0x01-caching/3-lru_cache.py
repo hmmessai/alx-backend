6eab4477c3d987ccc63dc9ea18a39bf51b6efdd9
Messai <messai@github.com>
2023-10-25 10:16:04 +0300

    3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Defines LRUCache class
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Represents LRUCahe object's properties and methods
+    """
+    def __init__(self):
+        """Initializes instance with basic properties
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns item to cache_data with the given key
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data.keys():
+                    del self.cache_data[key]
+                    self.cache_data[key] = item
+                else:
+                    for k, v in self.cache_data.items():
+                        print("DISCARD: {}".format(k))
+                        del self.cache_data[k]
+                        self.cache_data[key] = item
+                        break
+            elif key in self.cache_data.keys():
+                del self.cache_data[key]
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves value contained in cache with given key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
