497eaf3a27042bcdbe0e92b178c719ecbaea99d7
Messai <messai@github.com>
2023-10-31 16:39:35 +0300

    task 2 done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,11 +15,16 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+@babel.localeselector
+def get_locale():
+    """Gets the locale"""
+    return request.accept_languages.best_match(Config['LANGUAGES'])
+
 
 @app.route('/', strict_slashes=False)
 def home() -> str:
     """Serves the index.html file for home"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
