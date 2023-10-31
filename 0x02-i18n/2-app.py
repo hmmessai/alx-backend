a8a188dc40322422cade708d64d751dc76c2dd1a
Messai <messai@github.com>
2023-10-31 16:44:43 +0300

    1

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     """Gets the locale"""
@@ -28,4 +29,4 @@ def home() -> str:
 
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0, port=5000)
+    app.run(host='0.0.0.0', port=5000)
